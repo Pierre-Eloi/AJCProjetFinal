da97e857d8b1479759500d621f75d2cd2b946dea
Pierre-Eloi <pe.ragetly@gmail.com>
2023-09-01 09:44:27 +0200

    Add login and signup components

--- projet_react/src/App.tsx
+++ projet_react/src/App.tsx
@@ -7,7 +7,9 @@ import Liste from "./components/Liste";
 import Article from "./components/Article";
 import NoPage from "./components/NoPage";
 import Accueil from "./components/Accueil";
-
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   const showNavBar = window.location.pathname !== "/";
@@ -23,22 +25,24 @@ function App() {
   console.log("something else")
   return (
     <>
-    <BrowserRouter>
-    <div className="App">
-    {showNavBar && <SideBar cart={cart} setCart={setCart}/>}
-      <body>
-      <Routes>
-          <Route path="/" element={<LeMain />} />
-          <Route path="*" element={<NoPage />} />
-          <Route path="Accueil" element={<Accueil />} />
-          <Route path="SideBar" element={<SideBar cart={cart}  setCart={setCart}/>} />
-          <Route path="Liste" element={<Liste setCart={setCart} />} />
-          <Route path="Article/:id" element={<Article setCart={setCart}/>} />
-      </Routes> 
-      </body>
-      
-    </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="App">
+          {showNavBar && <SideBar cart={cart} setCart={setCart} />}
+          <body>
+            <Routes>
+              <Route path="/" element={<LeMain />} />
+              <Route path="*" element={<NoPage />} />
+              <Route path="Accueil" element={<Accueil />} />
+              <Route path="SideBar" element={<SideBar cart={cart} setCart={setCart} />} />
+              <Route path="Liste" element={<Liste setCart={setCart} />} />
+              <Route path="Article/:id" element={<Article setCart={setCart} />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<SignUp />} />
+            </Routes>
+            <Footer />
+          </body>
+        </div>
+      </BrowserRouter>
     </>
   );
 }

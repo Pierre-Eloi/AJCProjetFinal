65f18063e7961ab6d48f02ce3a34ff527c599b67
Pierre-Eloi <pe.ragetly@gmail.com>
2023-09-05 19:29:06 +0200

    close sidebar when click on a link

--- projet_react/src/components/UserSideBar.jsx
+++ projet_react/src/components/UserSideBar.jsx
@@ -7,37 +7,22 @@ const UserSideBar = (props) => {
     sessionStorage.removeItem('client');
     props.setIsOpen(false);
   }
-  return (
-    
-    <>
-    {/* <div className="container-fluid mt-3">
-    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-md">
-        <div className="container-fluid p-2">
-          <div className="form-inline">
-            <div className="btn btn-dark" onClick={ToggleSidebar} >
-              <i className="fa fa-user"></i>
-            </div>
-          </div>
-        </div>
-      </nav> */}
+  return (    
     <div className="sd ml-auto">
-        <div className="sd-header">
-          <div className="closebtn" onClick={() => props.setIsOpen(false)}>&#x2715;</div>
-          <h4>{props.prenom} {props.nom}</h4>
-        </div>
-        <div className="sd-body">
-          <Link to="/UserInfo">Mes informations</Link>
-          <hr />
-          <Link to="/UserCommandes">Mes commandes</Link>
-          <hr />          
-          <Link to="/Accueil" onClick={handleSignOut}>
-            <i className="fa fa-power-off me-3" aria-hidden="true"></i>Déconnexion
-          </Link>
-        </div>
+      <div className="sd-header">
+        <div className="closebtn" onClick={() => props.setIsOpen(false)}>&#x2715;</div>
+        <h4>{props.prenom} {props.nom}</h4>
       </div>
-    </>
-      
-      
+      <div className="sd-body">
+        <Link to="/UserInfo" onClick={() => props.setIsOpen(false)}>Mes informations</Link>
+        <hr />
+        <Link to="/UserCommandes" onClick={() => props.setIsOpen(false)}>Mes commandes</Link>
+        <hr />
+        <Link to="/Accueil" onClick={handleSignOut}>
+          <i className="fa fa-power-off me-3" aria-hidden="true"></i>Déconnexion
+        </Link>
+      </div>
+    </div>
   )
 };
 

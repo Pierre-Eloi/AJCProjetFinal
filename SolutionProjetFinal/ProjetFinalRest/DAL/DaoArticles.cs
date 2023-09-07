fd0c642855d33ce9e1a00a66d07d23f2e2904e17
Pierre-Eloi <pe.ragetly@gmail.com>
2023-09-07 09:44:27 +0200

    rest service, final version

---
+++ SolutionProjetFinal/ProjetFinalRest/DAL/DaoArticles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetFinalRest.DAL
+{
+    public class DaoArticles
+    {
+        public IEnumerable<Articles> FindAll()
+        {
+            return new ProjetFinalDBEntities().Articles.ToList<Articles>();
+        }
+
+        public Articles FindById(string id)
+        {
+            return new ProjetFinalDBEntities().Articles.Find(id);
+        }
+
+        public Articles Create(Articles article)
+        {
+            var context = new ProjetFinalDBEntities();
+            context.Articles.Add(article);
+            context.SaveChanges();
+            return article;
+        }
+
+        public Articles Delete(string id)
+        {
+            var context = new ProjetFinalDBEntities();
+            var article = context.Articles.Find(id);
+            context.Articles.Remove(article);
+            context.SaveChanges();
+            return article;
+        }
+
+        public Articles Update(Articles article)
+        {
+            var context = new ProjetFinalDBEntities();
+            context.Entry(article).State = EntityState.Modified;
+            context.SaveChanges();
+            return article;
+        }
+    }
+}
---
+++ SolutionProjetFinal/ProjetFinalRest/DAL/DaoCommandes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetFinalRest.DAL
+{
+    public class DaoCommandes
+    {
+        public IEnumerable<Commandes> FindbyIdClient(int idClient)
+        {
+            var context = new ProjetFinalDBEntities();
+            var commandes = context.Commandes.Where(c => c.IdClient == idClient);
+            return commandes;
+        }
+
+        public IEnumerable<Commandes> FindAll()
+        {
+            return new ProjetFinalDBEntities().Commandes.ToList<Commandes>();
+        }
+
+        public Commandes FindById(int id)
+        {
+            return new ProjetFinalDBEntities().Commandes.Find(id);
+        }
+
+        public Commandes Create(Commandes commande)
+        {
+            var context = new ProjetFinalDBEntities();
+            context.Commandes.Add(commande);
+            context.SaveChanges();
+            return commande;
+        }
+
+        public Commandes Delete(int id)
+        {
+            var context = new ProjetFinalDBEntities();
+            var commande = context.Commandes.Find(id);
+            context.Commandes.Remove(commande);
+            context.SaveChanges();
+            return commande;
+        }
+
+        public Commandes Update(Commandes commande)
+        {
+            var context = new ProjetFinalDBEntities();
+            context.Entry(commande).State = EntityState.Modified;
+            context.SaveChanges();
+            return commande;
+        }
+    }
+}
